c312ab57b2d584c80181fa2be4b854bc635a4b2b
Samet Tanrıkulu <73981818+Smt52@users.noreply.github.com>
2024-09-07 12:48:55 +0300

    Add domain exception handling and factory methods
    Updated GlobalUsing.cs to include Ordering.Domain.Exceptions.
    Added Create factory method in Customer.cs to validate parameters.
    Modified CustomerId.cs to include private constructor and static Of method.
    Introduced DomainException.cs for custom domain exceptions.

---
+++ src/Services/Ordering/Ordering.Domain/Exceptions/DomainException.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.Exceptions
+{
+    internal class DomainException : Exception
+    {
+        public DomainException()
+        {
+        }
+
+        public DomainException(string? message) : base($"Domain Exception: {message} throws for Domain Layer")
+        {
+        }
+
+        public DomainException(string? message, Exception? innerException) : base($"Domain Exception: {message} throws for Domain Layer and innerException is:{innerException}")
+        {
+        }
+
+        public string ToJson()
+        {
+            return JsonSerializer.Serialize(new
+            {
+                Message = Message,
+                StackTrace = StackTrace,
+            });
+        }
+
+        public static DomainException FromJson(string json)
+        {
+            var data = JsonSerializer.Deserialize<Dictionary<string, string>>(json);
+            var exception = new DomainException(data["Message"]);
+            return exception;
+        }
+    }
+}
--- src/Services/Ordering/Ordering.Domain/Models/Customer.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Customer.cs
@@ -5,5 +5,21 @@
         public string Name { get; set; } = default!;
 
         public string Email { get; set; } = default!;
+
+        //Factory method for customer
+        public static Customer Create(CustomerId id, string name, string email)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(name);
+            ArgumentException.ThrowIfNullOrWhiteSpace(email);
+
+            var customer = new Customer
+            {
+                Id = id,
+                Name = name,
+                Email = email
+            };
+
+            return customer;
+        }
     }
 }
