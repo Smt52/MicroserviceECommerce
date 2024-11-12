cd578ba086af3ce6a507229c7adfb1ec4a0f4f1c
Samet Tanrıkulu <73981818+Smt52@users.noreply.github.com>
2024-11-12 16:35:14 +0300

    Pagination Method Added

---
+++ src/Blocks/CommonOperations/Pagination/PaginatedResult.cs
@@ -0,0 +1,10 @@
+﻿namespace CommonOperations.Pagination;
+
+public class PaginatedResult<TEntity>
+    (int pageNumber,int pageSize,long count,IEnumerable<TEntity> data) where TEntity : class
+{
+    public int PageNumber { get; set; } = pageNumber;
+    public int PageSize { get; set; } = pageSize;
+    public long Count { get; set; } = count;
+    public IEnumerable<TEntity> Data { get; set; } = data;
+}
---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrders/GetOrdersHandler.cs
@@ -0,0 +1,29 @@
+﻿using CommonOperations.Pagination;
+
+namespace Ordering.Application.Orders.Queries.GetOrders;
+
+public class GetOrdersHandler(IApplicationDbContext dbContext):IQueryHandler<GetOrdersQuery,GetOrdersResult>
+{
+
+    public async Task<GetOrdersResult> Handle(GetOrdersQuery query, CancellationToken cancellationToken)
+    {
+        var pageNumber = query.PaginationRequest.PageNumber;
+        var pageSize = query.PaginationRequest.PageSize;
+        var totalCount = await dbContext.Orders.LongCountAsync(cancellationToken);
+        var orders = await dbContext.Orders
+            .Include(o => o.OrderItems)
+            .AsNoTracking()
+            .Skip(pageSize * pageNumber)
+            .Take(pageSize)
+            .ToListAsync(cancellationToken: cancellationToken);
+
+        return new GetOrdersResult(
+            new PaginatedResult<OrderDto>(
+                pageNumber,
+                pageSize,
+                totalCount,
+                orders.ToOrderDtoList()
+                )
+            );
+    }
+}
