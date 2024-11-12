0bc251aa70253d698646826dd4e307fd42777e75
Samet Tanrıkulu <73981818+Smt52@users.noreply.github.com>
2024-11-12 15:19:28 +0300

    Delete Order Implemented

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderCommand.cs
@@ -0,0 +1,15 @@
+﻿namespace Ordering.Application.Orders.Commands.DeleteOrder;
+
+public record DeleteOrderCommand(Guid OrderId)
+    : ICommand<DeleteOrderResult>;
+    
+    
+public record DeleteOrderResult(bool IsSuccess);
+
+public class DeleteCommandValidator : AbstractValidator<DeleteOrderCommand>
+{
+    public DeleteCommandValidator()
+    {
+        RuleFor(x => x.OrderId).NotEmpty().WithMessage("Order Must Be Selected");
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderHandler.cs
@@ -0,0 +1,27 @@
+﻿namespace Ordering.Application.Orders.Commands.DeleteOrder;
+
+public class DeleteOrderHandler(IApplicationDbContext dbContext)
+    : ICommandHandler<DeleteOrderCommand,DeleteOrderResult>
+{
+    public async Task<DeleteOrderResult> Handle(DeleteOrderCommand command, CancellationToken cancellationToken)
+    {
+        
+        //Delete Order 
+        //Don't need to include entities and order has a pk so more performant query will be FindAsync
+        var order = await dbContext.Orders.FindAsync([OrderId.Of(command.OrderId)], cancellationToken);
+        if (order==null)
+        {
+            throw new OrderNotFoundException(command.OrderId);
+        }
+        dbContext.Orders.Remove(order);
+        //Save Changes
+       var successfulCount =  await dbContext.SaveChangesAsync(cancellationToken);
+        //Return Result
+        if (successfulCount>0)
+        {
+            return new DeleteOrderResult(true);
+        }
+
+        return new DeleteOrderResult(false);
+    }
+}
