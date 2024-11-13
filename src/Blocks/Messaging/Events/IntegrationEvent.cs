9a0bb1f412e3f7e539f9095e4db355fc511f5c1e
Samet Tanrıkulu <73981818+Smt52@users.noreply.github.com>
2024-11-13 15:47:45 +0300

    Added Messaging and configuration made for MassTransit.RabbitMQ

---
+++ src/Blocks/Messaging/Events/IntegrationEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace Messaging.Events;
+
+public record IntegrationEvent
+{
+    public Guid Id { get; set; }
+    public DateTime OccuredOn => DateTime.Now;
+    public string EventType => GetType().AssemblyQualifiedName;
+}
---
+++ src/Blocks/Messaging/Events/ShoppingCartCheckoutEvent.cs
@@ -0,0 +1,25 @@
+﻿namespace Messaging.Events;
+
+//Used primitive types for serialization or deserialization between services
+public record ShoppingCartCheckoutEvent : IntegrationEvent
+{
+    public string UserName { get; set; } = default!;
+    public Guid CustomerId { get; set; } = Guid.Empty!;
+    public decimal TotalPrice { get; set; } = default!;
+
+    //For Addresses
+    public string FirstName { get; set; } = default!;
+    public string LastName { get; set; } = default!;
+    public string EmailAddress { get; set; } = default!;
+    public string AddressLine { get; set; } = default!;
+    public string Country { get; set; } = default!;
+    public string State { get; set; } = default!;
+    public string ZipCode { get; set; } = default!;
+
+    //For Payment
+    public string CardName { get; set; } = default!;
+    public string CardNumber { get; set; } = default!;
+    public string Expiration { get; set; } = default!;
+    public string Cvv { get; set; } = default!;
+    public int PaymentMethod { get; set; } = default!;
+}
